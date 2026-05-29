59e4c46eb2ec69f8cd1775799bb8090e58d8a66f
perihelion-xanadu <33140049+perihelion-xanadu@users.noreply.github.com>
2026-05-29 13:23:09 -0500

    ci: fix eslint undef check for playwright config process global

--- prototype/eslint.config.js
+++ prototype/eslint.config.js
@@ -18,4 +18,10 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  {
+    files: ['*.config.js', 'playwright.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ])
